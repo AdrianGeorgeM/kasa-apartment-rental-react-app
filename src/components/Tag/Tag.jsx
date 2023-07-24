aefabdef1d9c48c114a47c31ef3b2c5abda9d286
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-24 21:48:01 +0100

    Update styles for tags

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,6 +1,8 @@
 import React, { useMemo } from 'react';
 import { useRouter } from 'next/router';
+
+import styles from './Tag.module.css';
 const Tag = ({ tag }) => {
-	return <span className='tag'>{tag}</span>;
+	return <span className={styles.tag}>{tag}</span>;
 };
 export default Tag;
