0bd954bd37eeea1edad86c576eaba9d463212021
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-24 18:41:18 +0100

    Add ApartmentDetails component for displaying apartment info

--- src/components/ApartmentDetails/ApartmentDetails.jsx
+++ src/components/ApartmentDetails/ApartmentDetails.jsx
@@ -1,59 +1,56 @@
 import React, { useMemo } from 'react';
 import Image from 'next/image';
 import data from '/src/data/data.json';
-import styles from './ApartmentDetails.module.css'; // Import the CSS module styles
+import styles from './ApartmentDetails.module.css';
+
+const STAR_RANGE = Array(5).fill(null);
+
+const Star = ({ filled }) => (
+	<Image
+		src={filled ? '/assets/star_filled.svg' : '/assets/star_blank.svg'}
+		alt='Star'
+		className={styles.star}
+		width={50}
+		height={50}
+	/>
+);
+
+const Tag = ({ tag }) => <span className={styles.tag}>{tag}</span>;
 
 const ApartmentDetails = ({ id }) => {
 	const apartment = useMemo(() => data.find((datas) => datas.id === id), [id]);
 
-	if (!apartment) {
-		// Render a not found message or redirect to a 404 page
-		return <div>Apartment not found</div>;
-	}
-
-	const range = Array(5).fill(null);
+	const {
+		title,
+		location,
+		tags,
+		host: { name, picture },
+		rating,
+	} = apartment;
 
 	return (
 		<div className={styles.apartment}>
 			<main>
 				<div className={styles.info}>
 					<div className={styles['info__header']}>
-						<h1 className={styles['info__header--title']}>{apartment.title}</h1>
-						<p className={styles['info__header--location']}>{apartment.location}</p>
+						<h1 className={styles['info__header--title']}>{title}</h1>
+						<p className={styles['info__header--location']}>{location}</p>
 						<div className={styles['intro__header--tags']}>
-							{apartment.tags.map((tag, index) => (
-								<span className={styles.tag} key={index}>
-									{tag}
-								</span>
+							{tags.map((tag, index) => (
+								<Tag key={`tag-${index}`} tag={tag} />
 							))}
 						</div>
 					</div>
 					<div className={styles['info__profile']}>
 						<div className={styles['info__host']}>
-							<p className={styles['info__host--name']}>{apartment.host.name}</p>
+							<p className={styles['info__host--name']}>{name}</p>
 							<div className={styles['info__host--picture']}>
-								<Image
-									src={`/assets/${apartment.host.picture}`}
-									alt='Host'
-									width={500}
-									height={500}
-								/>
+								<Image src={`/assets/${picture}`} alt='Host' width={500} height={500} />
 							</div>
 						</div>
 						<div className={styles['info__rating']}>
-							{range.map((_, index) => (
-								<Image
-									src={
-										index < apartment.rating
-											? '/assets/star_filled.svg'
-											: '/assets/star_blank.svg'
-									}
-									alt='Star'
-									className={styles.star}
-									key={index}
-									width={50}
-									height={50}
-								/>
+							{STAR_RANGE.map((_, index) => (
+								<Star key={`star-${index}`} filled={index < rating} />
 							))}
 						</div>
 					</div>
