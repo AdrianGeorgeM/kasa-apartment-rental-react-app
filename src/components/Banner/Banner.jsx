643ee504c2de95fd35686855117279ad1f29b05d
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-24 21:56:51 +0100

    Update Banner to check for page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './Banner.module.css';
 
-const Banner = React.memo(() => {
+const Banner = React.memo(({ page }) => {
+	const getImageSrc = () => {
+		return '/assets/home_banner.png';
+	};
+
 	return (
 		<div className={styles.bannerContainer}>
 			<div className={styles.bannerBackground}></div>
 			<Image
-				src='/assets/home_banner.png'
+				src={getImageSrc()}
 				alt='banner'
 				layout='fill'
 				objectFit='cover'
 				className={styles.bannerImage}
 			/>
-			<h1 className={styles.bannerText}>Home anywhere and everywhere</h1>
+			<h1 className={styles.bannerText}>
+				{page === 'about' ? '' : 'Home anywhere and everywhere'}
+			</h1>
 		</div>
 	);
 });
