c8b2e6f1bac8dca9b0524f84a141298a3f7920f0
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-25 12:55:10 +0100

    Improve Card and Heart Icon Interactions

--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -12,8 +12,8 @@ const Thumbnail = ({ id, cover, title }) => {
 				<button aria-label='Add to favorites' className={styles.favoriteButton}>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
-						viewBox='0 0 24 24'
-						class={styles.heartFill}
+						viewBox='0 0 23 23'
+						className={styles.heartFill}
 					>
 						<path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3 9.24 3 10.91 4.01 12 5.09 13.09 4.01 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z' />
 					</svg>
