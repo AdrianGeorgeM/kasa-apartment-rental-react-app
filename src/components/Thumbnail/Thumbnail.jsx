a0721edcb62a9f65d5038ebe96dfe653cdb22946
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-25 12:26:18 +0100

    Make it more effcient the Thumbnail component

--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Thumbnail.module.css';
 
 const Thumbnail = ({ id, cover, title }) => {
 	return (
 		<Link href={`/apartment/${id}`} className={styles.card} passHref>
-			<div className={styles.cardImage} style={{ backgroundImage: `url(${cover})` }}>
+			<div className={styles.cardImage}>
+				<Image src={cover} alt={title} layout='fill' objectFit='cover' quality={100} />
 				<div className={styles.cardTitle}>{title}</div>
-				<button className={styles.favoriteButton}>♥</button>
+				<button aria-label='Add to favorites' className={styles.favoriteButton} />
 			</div>
 		</Link>
 	);
